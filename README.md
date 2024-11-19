6a5326ccdf0f73110ce71b497e7b26926bd5f6db
Norah Alqassim <128930577+noraalqassim@users.noreply.github.com>
2024-11-19 21:27:51 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,59 @@
+# Temperature Converter
+
+Develop a temperature converter program in C# that converts temperature values between Fahrenheit and Celsius.
+
+## Level 1: Basic conversion
+
+Implements a TempConvert function to perform temperature conversions.
+
+- Function TempConvert should receive 2 parameters: the amount of temperature, and the original measurement unit
+- Function TempConvert should return the string, containing new temperature and new unit.
+
+```
+// Expected output
+TempConvert(32, "F") returns "0 C"
+TempConvert(100, "C") returns "212 F"
+```
+
+## Level 2: Additional requirement: Asking for user input
+
+- Modify the application to prompt the user for a temperature value and a scale (Celsius 'C' or Fahrenheit 'F').
+
+```
+// Expected output
+Enter a temperature and its unit (C or F):
+> 45 F
+Converted: 45 F = 7.22 C
+
+```
+
+### Level 3: Advanced requirement: Continuous interaction
+
+- Implement comprehensive error handling for invalid inputs (non-numeric values, unsupported scales).
+- Modify the program to continuously prompt the user for input until the user enters `Quit.
+
+```
+// Expected output
+Enter a temperature and its unit (C or F), or type 'Quit' to exit:
+> 30 X
+Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.
+
+Enter a temperature and its unit (C or F), or type 'Quit' to exit:
+> forty-five C
+Invalid input. Please enter a numeric temperature.
+
+Enter a temperature and its unit (C or F), or type 'Quit' to exit:
+> 45 C
+Converted: 45 C = 113 F
+
+Enter a temperature and its unit (C or F), or type 'Quit' to exit:
+> Quit
+Program terminated.
+
+```
+
+## Screenshots of your output
+
+You should run your application and attach the screenshot image of your output (terminal) here.
+
+## **DEADLINE: 9am Tuesday 27.8**
---
+++ src/Program.cs
@@ -0,0 +1,70 @@
+﻿class Program
+{
+    //Basic conversion
+    static string TempConvert(double temperature, string originalUnit)
+    {
+        double convertedTemp = 0;
+        string newUnit;
+        //To convert from Celsius to Fahrenheit: F = (C * 9) / 5 + 32;
+        if (originalUnit == "C")
+        {
+            convertedTemp = temperature * 9 / 5 + 32;
+            newUnit = "F";
+            } 
+        //To convert from Fahrenheit to Celsius: C = (F – 32) * 5 / 9;
+        else 
+        {
+            convertedTemp = (temperature - 32) * 5 / 9;
+            newUnit = "C";
+        }
+
+        return $"{convertedTemp.ToString()} {newUnit}";
+    }
+    
+    static void Main()
+    {
+      //Level 1: Basic conversion
+        Console.WriteLine("Level 1 Output ");
+        Console.WriteLine(TempConvert(32, "F")); 
+        Console.WriteLine(TempConvert(100, "C")); 
+
+        Console.WriteLine("Level 2 and 3 Output ");
+      //Level 2: Additional requirement: Asking for user input
+        while (true)
+        {
+            Console.WriteLine("Enter a temperature and its unit (C or F), or type 'Quit' to exit:");
+            string input = Console.ReadLine();
+        //Level 3: Advanced requirement: Continuous interaction
+            if (input.Equals("Quit", StringComparison.OrdinalIgnoreCase)) //I use "StringComparison.OrdinalIgnoreCase" method becuse I don't know what the user will enter "Quit", "quit" or "QUIT".
+            {
+                Console.WriteLine("Program terminated.");
+                break;
+            }
+
+            string[] inputParts = input.Split(' '); //Split the string into two parts
+
+            if (inputParts.Length != 2)
+            {
+                Console.WriteLine("Invalid input format.");
+                continue; 
+            }
+
+            if (!double.TryParse(inputParts[0],
+                    out var temperature)) //"double.TryParse" method : to conver string to double number
+            {
+                Console.WriteLine("**Invalid input**. Please enter a numeric temperature.");
+                continue; 
+            }
+
+            var unit = inputParts[1].ToUpper();
+            if (unit != "C" && unit != "F")
+            {
+                Console.WriteLine("**Invalid scale**. Please enter 'C' for Celsius or 'F' for Fahrenheit.");
+                continue; 
+            }
+
+            var result = TempConvert(temperature, unit);
+            Console.WriteLine($"Converted: {input} = {result}");
+        }
+    }
+}
